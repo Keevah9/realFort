16fccdcec4f0f899f4e2f868fdabb531faca7794
Ogunniyi Veronica <86062530+Keevah9@users.noreply.github.com>
2022-09-11 21:53:34 +0100

    added new features

--- components/City.jsx
+++ components/City.jsx
@@ -4,7 +4,7 @@ const City = ({filter}) => {
   return (
     <>
               {filter?.city?.map((item) => (
-              <option value={item.value} className={`border-2 cursor-pointer px-4 py-2 `} key={item.value}>
+              <option value={item.value} className={`border-2 cursor-pointer  py-2 `} key={item.value}>
                 {item.name }
                 
               </option>
